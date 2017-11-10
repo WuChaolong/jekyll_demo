58158413114ece1af2da72ebd017bf131530ed96
Taifu <752761705@qq.com>
2017-11-10 18:32:37 +0800

    mdb

---
+++ freelancer/MDB Free/js/nav.js
@@ -0,0 +1,54 @@
+init();
+function init(){
+    var postion = readCookie("nav-postion")||"top";
+    setNav(postion);
+    onSettings(postion);
+}
+function onSettings(postion){
+    var radios = document.getElementsByName("positionRadios");
+    
+    var prev = null;
+    for(var i = 0;i<radios.length;i++){
+        radios[i].onclick = function() {
+            (prev)? console.log(prev.value):null;
+            if(this !== prev) {
+                prev = this;
+                setNav(this.value);
+            }
+            console.log(this.value)
+        };
+        if(radios[i].value==postion){
+            radios[i].checked = true;
+        }
+    }
+}
+function setNav(postion){
+     document.body.classList=[];
+     document.body.classList.add("nav-"+postion);
+     createCookie("nav-postion",postion);
+}
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
